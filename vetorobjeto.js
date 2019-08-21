8babd3c80e46b307980130fd9fa24ba3b632bf10
Thiago forneli silveira <silveiraforneli@gmail.com>
2019-08-21 20:34:16 -0300

    exemplo de vetor de objetos

---
+++ vetorobjeto.js
@@ -0,0 +1,21 @@
+function funcao(){
+    //Cria o vetor:
+    var vet = []
+    //Cria o objeto
+    var objeto = {}
+    //Cria os produtos
+    for(var i = 0; i<5; i++){
+        objeto.codigo = Number(prompt("Informe o código: "));
+        objeto.nome = prompt("Informe o nome: ");
+        objeto.qntd = Number(prompt("Informe a quantidade: "));
+        objeto.preco = Number(prompt("Informe o preço: "));
+        vet.push(objeto)
+        alert("Produto criado com sucesso!")
+
+    }
+    var estoque = 0;
+    for(i=0;i<5;i++){
+        estoque = estoque + vet[i].qntd
+    }
+    alert("Quantidade em um estoque: " + estoque)
+}
